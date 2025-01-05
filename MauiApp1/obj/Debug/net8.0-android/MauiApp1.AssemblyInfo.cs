6baac4f238306fdf49ad45f8e6932245e85bb669
MohammadAmmarJ <149337449+MohammadAmmarJ@users.noreply.github.com>
2025-01-05 19:12:00 +0200

    Release version V1.0

--- MauiApp1/obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
+++ MauiApp1/obj/Debug/net8.0-android/MauiApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d485df24e0d1be21afdbe2c48dc877121e110562")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ca325e379b43d4d651c571858860549b0ac36568")]
 [assembly: System.Reflection.AssemblyProductAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MauiApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
